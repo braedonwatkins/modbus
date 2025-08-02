0f8f929d480866ccf86923e25df6acc791b8465d
braedonwatkins <watkins.braedon@gmail.com>
2025-08-02 19:32:42 -0400

    stopping point for readcoils func
    I think I actually want to create a Res abstraction because sending some
    random mix of data back is killing me and I imagine with other functions
    it will only get more confusing...

--- functions.go
+++ functions.go
@@ -4,17 +4,19 @@ import "fmt"
 
 const MAX_READ_COILS = 0x7D0
 
-func handleReadCoils(hw *Hardware, startAddress uint16, quantity uint16) ([]bool, error) {
+func handleReadCoils(hw *Hardware, startAddress uint16, quantity uint16) ([]byte, byte, error) {
 	hw.mu.RLock()
 	defer hw.mu.RUnlock()
 
 	if quantity == 0 || quantity > MAX_READ_COILS {
-		return nil, fmt.Errorf("Number of coils should be between 0 and %d, got %d", MAX_READ_COILS, quantity)
+		return nil, 0, fmt.Errorf("Number of coils should be between 0 and %d, got %d", MAX_READ_COILS, quantity)
 	}
 
 	endAddress := startAddress + quantity
-	res := hw.coils[startAddress]
+	bits := hw.coils[startAddress:endAddress]
+	res, numBytes := PackBits(bits)
 
+	return res, numBytes, nil
 }
 
 func handleReadDiscreteInputs()             {}
